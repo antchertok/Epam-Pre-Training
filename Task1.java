71984acbd29cd99aad60fad27a89bbce88e08dd0
antchertok <35873231+antchertok@users.noreply.github.com>
2018-01-28 15:56:25 +0300

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -1,16 +1,16 @@
 public class Task1 {
     
-    //    1
+    //    1 Граммы в тонны
     static double getTons(long grams) {
         return grams / 1_000_000;
     }
 
-    //    2
+    //    2 Площадь кольца
     static double getCircleSquare(double r1, double r2) {
         return 3.14 * (r1 * r1 - r2 * r2);
     }
 
-    //    3
+    //    3 Поменять местами содержимое переменных
     static void changeStorage(double a, double b) {
         a = a - b;
         b = b + a;
@@ -18,7 +18,7 @@ public class Task1 {
         System.out.println(a + " " + b);
     }
 
-    //    4
+    //    4 Являются ли числа в чиле возрастающей последовательностью
     static boolean isIncreasingSequence(int n) {
         int digit;
         boolean isIncreasing = true;
@@ -32,7 +32,7 @@ public class Task1 {
         return isIncreasing;
     }
 
-    //    5.1
+    //    5.1 Сумма цифр в числе
     static int calcSum(int n) {
         int sum = 0;
         for (int i = 0; i < 5; i++) {
@@ -42,7 +42,7 @@ public class Task1 {
         return sum;
     }
 
-    //    5.2
+    //    5.2 Произведение цифр в числе
     static int calcMult(int n) {
         int mult = 1;
         for (int i = 0; i < 5; i++) {
@@ -52,7 +52,7 @@ public class Task1 {
         return mult;
     }
 
-    //    6.1
+    //    6.1 Среднее арифметическое цифр в числе
     static double calcArithAverage(int n) {
         double average;
         int sum = 0;
@@ -64,7 +64,7 @@ public class Task1 {
         return average;
     }
 
-    //    6.2
+    //    6.2 Среднее геометрическое цифр в числе
     static double calcGeomAverage(int n) {
         double average;
         int mult = 1;
@@ -76,7 +76,7 @@ public class Task1 {
         return average;
     }
 
-    //    7
+    //    7 Число с цифрами в обратном порядке
     static int reverse(int n) {
         int reversed = 0;
         for (int i = 0; i < 7; i++) {
