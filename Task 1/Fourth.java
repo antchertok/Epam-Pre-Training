727aa22e85c4273049e131ea65db485ef32717ea
antchertok <35873231+antchertok@users.noreply.github.com>
2018-01-30 11:31:28 +0300

    Update Fourth.java

--- Task 1/Fourth.java
+++ Task 1/Fourth.java
@@ -2,6 +2,7 @@ public class Fourth {
     static boolean isIncreasingSequence(int n) {
         int digit;
         boolean isIncreasing = true;
+        
         digit = n % 10;
         n /= 10;
         isIncreasing = (digit <= (n % 10) || !isIncreasing) ? false : true;
