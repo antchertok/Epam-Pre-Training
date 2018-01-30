67714aced30123121bb50ad6d82be29aa0759f25
antchertok <35873231+antchertok@users.noreply.github.com>
2018-01-30 11:43:12 +0300

    Update Fifth.java

--- Task 1/Fifth.java
+++ Task 1/Fifth.java
@@ -1,7 +1,10 @@
 public class Fifth {
+    
     static int calcSum(int n) {
+        int digit;
         int sum = 0;
-        int digit = n % 10;
+        
+        digit = n % 10;
         sum += digit;
         n /= 10;
 
@@ -22,8 +25,10 @@ public class Fifth {
     }
 
     static int calcMult(int n) {
+        int digit;
         int mult = 1;
-        int digit = n % 10;
+        
+        digit = n % 10;
         mult *= digit;
         n /= 10;
 
