e6a92010b169ceb962ddc17600b5c288a87eefa1
antchertok <35873231+antchertok@users.noreply.github.com>
2018-01-30 13:05:11 +0300

    Update Seventh.java

--- Task 1/Seventh.java
+++ Task 1/Seventh.java
@@ -1,18 +1,26 @@
 public class Seventh {
+    
     static int reverse(int n) {
         int reversed = 0;
+        
         reversed = reversed * 10 + n % 10;
         n /= 10;
+        
         reversed = reversed * 10 + n % 10;
         n /= 10;
+        
         reversed = reversed * 10 + n % 10;
         n /= 10;
+        
         reversed = reversed * 10 + n % 10;
         n /= 10;
+        
         reversed = reversed * 10 + n % 10;
         n /= 10;
+        
         reversed = reversed * 10 + n % 10;
         n /= 10;
+        
         reversed = reversed * 10 + n;
         return reversed;
     }
