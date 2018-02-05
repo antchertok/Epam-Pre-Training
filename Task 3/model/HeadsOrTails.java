bb3e44497cec52299209d46ce8bd45030d4c570a
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-02-05 17:20:21 +0300

    Переделано

--- Task 3/model/HeadsOrTails.java
+++ Task 3/model/HeadsOrTails.java
@@ -3,42 +3,13 @@ package third.model;
 import java.util.Random;
 
 public class HeadsOrTails {
-    private static Random random = new Random();
+    private static Random coinThrower = new Random();
 
-    private enum HeadOrTail {HEAD, TAIL}
-
-    ;
-
-    public static String emulateHeadsOrTails(int amountOfFlings) {
-        int heads = 0;
-        int tails = 0;
-        for (int fling = 0; fling < amountOfFlings; fling++) {
-            switch (throwCoin()) {
-                case HEAD:
-                    heads++;
-                    break;
-                case TAIL:
-                    tails++;
-                    break;
-            }
-        }
-        return "Heads: " + heads + " Tails: " + tails;
-    }
-
-    private static HeadOrTail throwCoin() {
-        if (random.nextInt(2) == 0) {
-            return HeadOrTail.HEAD;
-        } else {
-            return HeadOrTail.TAIL;
-        }
-    }
-
-    //    Или так
-    public static int howManyHeads(int amountOfFlings) {
+    public static int howManyHeads(int amountOfTosses) {
         int heads = 0;
 
-        for (int fling = 0; fling < amountOfFlings; fling++) {
-            if (throwCoin() == HeadOrTail.HEAD) {
+        for (int toss = 0; toss < amountOfTosses; toss++) {
+            if (coinThrower.nextBoolean()) {
                 heads++;
             }
         }
