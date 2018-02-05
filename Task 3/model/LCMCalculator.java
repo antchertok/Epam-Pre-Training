e40cae1f8a606810b354ed49ae240bbcee256319
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-02-05 22:51:01 +0300

    Переделано

--- Task 3/model/LCMCalculator.java
+++ Task 3/model/LCMCalculator.java
@@ -2,24 +2,14 @@ package third.model;
 
 public class LCMCalculator {
 
-    public static int calcLeastCommonMultiple(int a, int b) {
+    //    Надо ли указывать непроверяемые исключения через throws?
+    public static int calcLCM(int a, int b) throws RuntimeException {
+        
+        if (a * b == 0) {
+            throw new RuntimeException("At least one number is zero");
+        }
+
         return Math.abs(a * b) / GCDCalculator.calcGCD(a, b);
     }
 
-//    Если нельзя способом выше
-    public static int calcLCM(int a, int b) {
-        a = a > 0 ? a : -a;
-        b = b > 0 ? b : -b;
-        a = a > b ? a : a + b - (b = a);
-        int finalNumerator = a * b;
-        int remainder;
-
-        while (a % b != 0) {
-            remainder = a % b;
-            a = b;
-            b = remainder;
-        }
-
-        return finalNumerator / b;
-    }
 }
