20e8ce985de6d79859cc1c49eca99846b8aebb00
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-02-05 22:00:37 +0300

    Переделано

--- Task 3/model/PrimeDivisorsDetector.java
+++ Task 3/model/PrimeDivisorsDetector.java
@@ -7,26 +7,11 @@ public class PrimeDivisorsDetector {
         StringBuilder primeDivisors = new StringBuilder();
 
         for (int divisor = MIN_PRIME_NUMBER; divisor < number; divisor++) {
-            if (isDivisor(number, divisor)){
-                if(isPrime(divisor)){
-                    primeDivisors.append(divisor);
-                    primeDivisors.append(' ');
-                }
+            if (number % divisor == 0 && PrimeNumIndicator.isPrime(divisor)) {
+                primeDivisors.append(divisor);
+                primeDivisors.append(' ');
             }
         }
         return primeDivisors.toString();
-    }
-
-    private static boolean isPrime(int number) {
-        for (int divisor = MIN_PRIME_NUMBER; divisor < number; divisor++) {
-            if (isDivisor(number,divisor)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static boolean isDivisor(int dividend, int divisor) {
-        return dividend % divisor == 0;
     }
 }
