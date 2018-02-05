11b431caa517ef155cc7bb5ae4b9bb5afdadccf9
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-02-05 22:31:33 +0300

    Переделано

--- Task 3/model/PalindromeIndicator.java
+++ Task 3/model/PalindromeIndicator.java
@@ -3,14 +3,16 @@ package third.model;
 public class PalindromeIndicator {
 
     public static boolean isPalindrome(int number) {
-        int reversed = 0;
         number = number > 0 ? number : -number;
+        return number == reverse(number);
+    }
+
+    private static int reverse(int number){
+        int reversed = 0;
 
         for (int n = number; n > 0; n /= 10) {
             reversed = reversed * 10 + n % 10;
         }
-
-        return number == reversed;
+        return reversed;
     }
-
 }
