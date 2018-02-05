ff9b3f6cdbbbbf2d21bc2fb9fe76d2f41f5ddf2e
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-02-05 22:48:47 +0300

    Дополнено

--- Task 3/model/PrimeDivisorsDetector.java
+++ Task 3/model/PrimeDivisorsDetector.java
@@ -4,6 +4,7 @@ public class PrimeDivisorsDetector {
     private static final int MIN_PRIME_NUMBER = 2;
 
     public static String getPrimeDivisors(int number) {
+        number = number > 0 ? number : -number;
         StringBuilder primeDivisors = new StringBuilder();
 
         for (int divisor = MIN_PRIME_NUMBER; divisor < number; divisor++) {
