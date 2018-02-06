c5af4eebcc6b223782f20141233f5411dfe84c0c
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-02-06 11:13:00 +0300

    Update DigitCounter.java

--- Task 3/model/DigitCounter.java
+++ Task 3/model/DigitCounter.java
@@ -3,14 +3,14 @@ package third.model;
 public class DigitCounter {
 
     public static int howManyDigits(int number) {
-        number = number > 0 ? number : -number;
-        int amountOfDigits = 0;
-
         if (number == 0) {
             return 1;
         }
 
-        for (; number > 0; number /= 10) {
+        number = number > 0 ? number : -number;
+        int amountOfDigits = 0;
+
+        for (; number > 1; number /= 10) {
             if (!contains(number / 10, number % 10)) {
                 amountOfDigits++;
             }
