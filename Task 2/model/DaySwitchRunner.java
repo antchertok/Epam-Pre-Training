69b755f43c3e465c8c4831873dbbc733515168dd
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-02-07 21:30:57 +0300

    Update DaySwitchRunner.java

--- Task 2/model/DaySwitchRunner.java
+++ Task 2/model/DaySwitchRunner.java
@@ -2,12 +2,14 @@ package second.model;
 
 public class DaySwitchRunner{
     public static String nextDay(int day, int month, int year){
-        String nextDay = null;
+        String nextDay = "Incorrect input";
+        
         try{
             nextDay =  DaySwitch.nextDay(day, month, year);
         } catch (InvalidDateException e){
             e.printStackTrace();
         }
+        
         return nextDay;
     }
 }
