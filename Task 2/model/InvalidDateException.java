9af1575108cb4fbb031b60d783dec8d0fc9653ce
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-02-07 21:42:51 +0300

    Update InvalidDateException.java

--- Task 2/model/InvalidDateException.java
+++ Task 2/model/InvalidDateException.java
@@ -1,4 +1,15 @@
 package second.model;
 
 public class InvalidDateException extends Exception{
+    public InvalidDateException(){
+    }
+    public InvalidDateException(String message, Throwable cause){
+        super(message, cause);
+    }
+    public InvalidDateException(String message){
+        super(message);
+    }
+    public InvalidDateException(Throwable cause){
+        super(cause);
+    }
 }
