825058e13887074a0a34e4bfb8ce9b6517008e55
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-02-08 19:28:09 +0300

    Update MoodDetectorRunner.java

--- Task 2/model/MoodDetectorRunner.java
+++ Task 2/model/MoodDetectorRunner.java
@@ -2,7 +2,7 @@ package second.model;
 
 public class MoodDetectorRunner {
     public static String detectMood(int amountOfMoods) {
-        String mood = "Incorrect Input";
+        String mood = "Incorrect Input";//Следует ли возвращать что-либо в случае исключения? Бросать RuntimeException?
 
         try {
             mood = MoodDetector.detectMood(amountOfMoods);
