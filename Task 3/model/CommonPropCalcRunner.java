80f5d381fb7742a02d96418150da035b4c0b37cc
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-02-12 20:17:38 +0300

    Update CommonPropCalcRunner.java

--- Task 3/model/CommonPropCalcRunner.java
+++ Task 3/model/CommonPropCalcRunner.java
@@ -2,7 +2,7 @@ package third.model;
 
 public class CommonPropCalcRunner {
     public static int calcGCD(int firstNumber, int secondNumber){
-        int gsd = 1;//Следует ли возвращать что-либо в случае исключения? Бросать RuntimeException?
+        int gsd = -1;//Следует ли возвращать что-либо в случае исключения? Бросать RuntimeException?
 
         try {
             gsd = CommonPropCalc.calcGCD(firstNumber, secondNumber);
@@ -14,7 +14,7 @@ public class CommonPropCalcRunner {
     }
 
     public static int calcLCM(int firstNumber, int secondNumber){
-        int lcm = 1;
+        int lcm = -1;
 
         try {
             lcm = CommonPropCalc.calcLCM(firstNumber, secondNumber);
