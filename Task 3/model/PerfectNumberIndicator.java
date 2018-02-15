81a34880e92357bf9bb4ff8a0fc6857bbed78782
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-02-15 23:37:02 +0300

    Update PerfectNumberIndicator.java

--- Task 3/model/PerfectNumberIndicator.java
+++ Task 3/model/PerfectNumberIndicator.java
@@ -2,8 +2,8 @@ package third.model;
 
 public class PerfectNumberIndicator {
 
-    public static boolean isPerfect(int number) {
-        if(number == 0){
+    public static boolean indicatePerfect(int number) {
+        if(number == 0 || number == 1){
             return false;
         }
 
@@ -12,9 +12,10 @@ public class PerfectNumberIndicator {
     }
 
     private static int calcSumOfDivisors(int number) {
-        int sum = 0;
+        int sum = 1;
+        int upperLimit = number / 2;
 
-        for (int divisor = 1; divisor < number; divisor++) {
+        for (int divisor = 2; divisor <= upperLimit; divisor++) {
             if (number % divisor == 0) {
                 sum += divisor;
             }
