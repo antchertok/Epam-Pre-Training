4e21f34368d21c838359e49723c8cb284375ad41
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-02-16 22:51:52 +0300

    Update TowerOfHanoi.java

--- Task 4/model/TowerOfHanoi.java
+++ Task 4/model/TowerOfHanoi.java
@@ -2,13 +2,15 @@ package by.epam.pretraining.forth.model;
 
 public class TowerOfHanoi {
 
-    public static String solve (int height, char src, char destination, char temp){
-        if (height == 0){
+    public static String solve(int height, char src, char destination, char temp) {
+        if (height == 0) {
             return "";
         }
+        
         height = height < 0 ? -height : height;
         return solvePuzzle(height, src, destination, temp).toString();
     }
+
     public static StringBuilder solvePuzzle(int height, char src, char destination, char temp) {
         StringBuilder step = new StringBuilder();
 
