ffac4c9349fd77e0de212ab30d21ab9f72da9636
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-02-26 23:15:56 +0300

    Add files via upload

--- Task 5/controller/Depot.java
+++ Task 5/controller/Depot.java
@@ -4,27 +4,26 @@ import by.epam.pretraining.chertok.tasks.task5.model.DepotMaker;
 import by.epam.pretraining.chertok.tasks.task5.model.entity.Train;
 import by.epam.pretraining.chertok.tasks.task5.model.logic.HeaviestTrainDetector;
 import by.epam.pretraining.chertok.tasks.task5.model.logic.LongestTrainDetector;
-import by.epam.pretraining.chertok.tasks.task5.model.logic.TrainStalker;
+import by.epam.pretraining.chertok.tasks.task5.model.logic.TrainFinder;
 import by.epam.pretraining.chertok.tasks.task5.model.util.Container;
 import by.epam.pretraining.chertok.tasks.task5.view.View;
 
 public class Depot {
 
     public static void main(String[] args) {
-
-        Container<Train> depot = DepotMaker.makeDepot(50, 2, 70);
+        Container<Train> depot = DepotMaker.DEPOT_MAKER.makeDepot(50, 2, 70);
 
         Train train = new Train((Train) depot.get(0));
         View.print(train);
         View.print(depot.get(0));
 
         View.print("TOTAL: " + depot.size());
-        View.print("GOODS: " + TrainStalker.findTrainsOfThisType(depot, "GOODS").size());
-        View.print("PASSENGER: " + TrainStalker.findTrainsOfThisType(depot, "PASSENGER").size());
+        View.print("GOODS: " + TrainFinder.findTrainsOfThisType(depot, "GOODS").size());
+        View.print("PASSENGER: " + TrainFinder.findTrainsOfThisType(depot, "PASSENGER").size());
 
         View.print(LongestTrainDetector.whichTrainIsLongest(depot));
-        View.print(TrainStalker.findTrainsWithThisDestination(depot, "SOFIA").size());
-        View.print(TrainStalker.findTrainsWithThisTypeOfLoco(depot, "SteAm").size());
+        View.print(TrainFinder.findTrainsWithThisDestination(depot, "SOFIA").size());
+        View.print(TrainFinder.findTrainsWithThisTypeOfLoco(depot, "SteAm").size());
         View.print(HeaviestTrainDetector.whichTrainIsHeaviest(depot));
 
         View.print(depot.toString());
