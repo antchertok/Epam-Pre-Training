b9a1558128cced3ade2831c646e031657ab563e1
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-03-01 12:20:13 +0300

    Update Test.java

--- Task 6/controller/Test.java
+++ Task 6/controller/Test.java
@@ -5,6 +5,6 @@ import sixth.model.PalindromeIndicator;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println(PalindromeIndicator.indicatePalindrome("BadaB"));
+        View.print(PalindromeIndicator.indicatePalindrome("BadaB"));
     }
 }
