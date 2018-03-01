51580ef64c7ada4d6d689c4c2311616885da46b7
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-03-01 13:10:39 +0300

    Add files via upload

--- Task 7/controller/Test.java
+++ Task 7/controller/Test.java
@@ -1,26 +1,32 @@
 package by.epam.pretraining.chertok.tasks.task7.controller;
 
+import by.epam.pretraining.chertok.tasks.task7.model.NoSuchElementException;
 import by.epam.pretraining.chertok.tasks.task7.model.Tree;
+import by.epam.pretraining.chertok.tasks.task7.model.TreeMaker;
 import by.epam.pretraining.chertok.tasks.task7.view.View;
 
 public class Test {
 
     public static void main(String[] args) {
-        Tree<Double> tree = new Tree<>();
-        double removing = 0;
-        for (int i = 0; i < 30; i++) {
-            double element = Math.random() * 100;
-            tree.add(element);
-            if(i == 25){
-                removing = element;
-            }
-        }
+        Tree<Double> tree = TreeMaker.TREE_MAKER.makeTreeDouble(20);
+
+        double removing = 0.3141593;
+
         View.print(tree.size());
-        View.print(tree.getAll());
         View.print(tree.first());
+        tree.add(removing);
+        View.print(tree.getAll());
 
         View.print(tree.contains(removing));
-//        TreeRemoveRunner.REMOVE_RUNNER.remove(removing);
+        View.print(tree.size());
+        
+//        Исправлю в ближайшее время
+        try{
+            tree.remove(removing);
+        }catch (NoSuchElementException e){
+            e.printStackTrace();
+        }
+
         View.print(tree.size());
         View.print(removing);
         View.print(tree.contains(removing));
