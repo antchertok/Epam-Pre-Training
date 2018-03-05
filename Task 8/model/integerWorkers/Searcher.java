88b26f8ef308e23f8531df0901d49184f489f2bd
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-03-05 10:47:58 +0300

    Add files via upload

--- Task 8/model/integerWorkers/Searcher.java
+++ Task 8/model/integerWorkers/Searcher.java
@@ -1,24 +1,22 @@
 package by.epam.pretraining.chertok.tasks.task8.model.integerWorkers;
 
-import by.epam.pretraining.chertok.tasks.task5.model.entity.Train;
-
 import java.util.Arrays;
 
 public enum Searcher {
     SEARCHER;
 
-    public boolean containsLinear(Train element, Train... elements) {
+    public boolean containsLinear(Integer element, Integer... elements) {
         return element.equals(elements[0]) || containsLinearRecursion(element, 1, elements);
     }
 
-    public boolean containsLinearRecursion(Train element, int startPos, Train... elements) {
+    public boolean containsLinearRecursion(Integer element, int startPos, Integer... elements) {
         return startPos == elements.length - 1 || element.equals(elements[startPos])
                 ? element.equals(elements[startPos])
                 : containsLinearRecursion(element, ++startPos, elements);
     }
 
-    public boolean containsLinearIterate(Train element, Train... elements) {
-        for (Train el : elements) {
+    public boolean containsLinearIterate(Integer element, Integer... elements) {
+        for (Integer el : elements) {
             if (element.equals(el)) {
                 return true;
             }
@@ -26,7 +24,7 @@ public enum Searcher {
         return false;
     }
 
-    public boolean containsBinary(Train element, Train[] sorted) {
+    public boolean containsBinary(Integer element, Integer[] sorted) {
         int central = sorted.length / 2;
         if (sorted.length == 1) {
             return element.equals(sorted[central]);
@@ -38,19 +36,19 @@ public enum Searcher {
         }
     }
 
-    public boolean containsBinaryIterate(Train element, Train[] sorted) {
+    public boolean containsBinaryIterate(Integer element, Integer[] sorted) {
         int range = sorted.length;
         int central = range / 2;
 
         while (central > 0 && !element.equals(sorted[central])) {
             if (element.compareTo(sorted[central]) < 0) {
                 central = (range /= 2) / 2;
-                Train[] temp = new Train[range];
+                Integer[] temp = new Integer[range];
                 System.arraycopy(sorted, 0, temp, 0, range);
                 sorted = temp;
             } else {
                 range = range % 2 == 0 ? range / 2 : range / 2 + 1;
-                Train[] temp = new Train[range];
+                Integer[] temp = new Integer[range];
                 System.arraycopy(sorted, central, temp, 0, range);
                 central = range / 2;
                 sorted = temp;
