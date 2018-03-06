96271db62064eec318b79bca224108242c3e5761
Anton Chertok <35873231+antchertok@users.noreply.github.com>
2018-03-06 21:21:38 +0300

    Delete AbstractData.java

--- Task 6/model/AbstractData.java
+++
@@ -1,9 +0,0 @@
-package by.epam.pretraining.chertok.tasks.task6.model;
-
-public interface AbstractData<T> {
-    int size();
-
-    T peek() throws ContainerIsEmptyException;
-
-    boolean isEmpty();
-}
